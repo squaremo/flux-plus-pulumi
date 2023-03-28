d6697c35cb6a683bcedcebbe736d4e9f3fa1249b
Michael Bridgen <mikeb@squaremobius.net>
2023-03-28 16:41:45 +0100

    Add note on using syncs to install Pulumi-op
    Signed-off-by: Michael Bridgen <mikeb@squaremobius.net>

--- infra/index.js
+++ infra/index.js
@@ -37,35 +37,19 @@ const applyFluxYAMLs = new k8s.yaml.ConfigGroup('flux-apply',
                                                 { yaml: [installFlux.content, syncFlux.content] },
                                                 { provider: cluster });
 
-  // # Create a Kubernetes provider (cluster client, pretty much), and use it to
-  // # install Flux and the Pulumi operator.
-  
-  // kubernetes:
-  //   type: pulumi:providers:kubernetes
-  //   properties:
-  //     kubeconfig: kubeconfig
-          
-  // flux-install:
-  //   type: kubernetes:yaml:ConfigGroup
-  //   properties:
-  //     yaml: ${fluxInstall.content}
-  //   options:
-  //     provider: kubernetes
+// I _could_ use another ConfigGroup to install the Pulumi operator --
+// but, since I've got Flux now, I can just drop some syncs into the
+// git repository, instead.
 
-  // # There are example Pulumi programs for deploying the operator, but
-  // # they are just these YAMLs transliterated into various
-  // # languages. There's no Helm chart (though Engin has created one, I
-  // # just never got to review & merge it).
-  // pulumi-install:
-  //   type: kubernetes:yaml:ConfigGroup
-  //   properties:
-  //     files:
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/crds/pulumi.com_stacks.yaml
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/crds/pulumi.com_programs.yaml
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/crds/pulumi.com_programs.yaml
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/yaml/service_account.yaml
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/crds/role.yaml
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/crds/role_binding.yaml
-  //     - https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/crds/operator.yaml
-  //   options:
-  //     provider: kubernetes
+// const pulumiOperator = new k8s.yaml.ConfigGroup('pulumi-apply',
+//                                                 {
+//                                                     files: [
+//                                                         'crds/pulumi.com_stacks.yaml',
+//                                                         'crds/pulumi.com_programs.yaml',
+//                                                         'yaml/service_account.yaml',
+//                                                         'yaml/role.yaml',
+//                                                         'yaml/role_binding.yaml',
+//                                                         'yaml/operator.yaml'
+//                                                     ].map(f => `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/master/deploy/${f}`)
+//                                                 },
+//                                                 { provider: cluster })
